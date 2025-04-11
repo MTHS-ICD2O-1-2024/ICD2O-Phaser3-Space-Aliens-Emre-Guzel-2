f6690c4083caecf9552d54a7d16782c1cde52087
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2025-04-11 17:02:38 +0000

    added window

--- js/game.js
+++ js/game.js
@@ -1,5 +1,18 @@
+/* global Phaser*/ 
+
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Emre Guzel
 // Created on: Apirl 11 2025
 // This file contains the JS functions for index.html
+
+// Setting the window 
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1000,
+  backgroundColor: 0x5f6e7a,
+}
+// Setting the gmae
+const game = new Phaser.Game()
+console.log(game)
