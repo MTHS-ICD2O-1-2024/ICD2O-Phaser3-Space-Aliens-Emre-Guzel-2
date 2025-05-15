a232ad590cf6ab18b62465edd565a3c8b1a9c93d
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2025-05-15 13:02:54 -0400

    added aliens

--- js/gameScene.js
+++ js/gameScene.js
@@ -1,86 +1,106 @@
 /* global Phaser */
-
 // Copyright (c) 2020 Mr Coxall All rights reserved
 //
-// Created by: Emre Guzel
-// Created on: May 5 2025
+// Created by: Emre
+// Created on: May 14 2025
 // This is the Game Scene
 
 /**
  * This class is the Game Scene.
  */
 class GameScene extends Phaser.Scene {
-  constructor() {
-    super({ key: 'gameScene' })
+  // create an alien
+  createAlien() {
+    const alienXLocation = Math.floor(Math.random() * 1920) + 1;
+    let alienXVelocity = Math.floor(Math.random() * 50) + 1;
+    alienXVelocity *= Math.round(Math.random()) ? 1 : -1;
 
-    this.background = null
-    this.ship = null
-    this.fireMissile = false
+    const anAlien = this.physics.add.sprite(alienXLocation, -100, "alien");
+    anAlien.body.velocity.y = 200;
+    anAlien.body.velocity.x = alienXVelocity;
+
+    this.alienGroup.add(anAlien);
+  }
+
+  constructor() {
+    super({ key: "gameScene" });
+
+    this.background = null;
+    this.ship = null;
+    this.fireMissile = false;
   }
 
   init(data) {
-    this.cameras.main.setBackgroundColor('#ffffff')
+    this.cameras.main.setBackgroundColor("ffffff");
   }
 
   preload() {
-    console.log('Game Scene')
+    console.log("Game Scene");
 
-    //images
-    this.load.image('starBackground', 'assets/starBackground.png')
-    this.load.image('ship', 'assets/spaceShip.png')
-    this.load.image('missile', 'assets/missile.png')
-    //sound
-    this.load.audio('laser', 'assets/laser1.wav')
+    // images
+    this.load.image("starBackground", "./assets/starBackground.png");
+    this.load.image("ship", "./assets/spaceShip.png");
+    this.load.image("missile", "./assets/missile.png");
+    this.load.image("alien", "./assets/alien.png");
+
+    // sound
+    this.load.audio("laser", "./assets/laser1.wav");
   }
 
   create(data) {
-    this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
-    this.background.setOrigin(0, 0)
+    this.background = this.add.image(0, 0, "starBackground").setScale(2.0);
+    this.background.setOrigin(0, 0);
 
-    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
+    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship");
 
-    this.missileGroup = this.physics.add.group()
+    // create a group for the missiles
+    this.missileGroup = this.physics.add.group();
+
+    // create a group for the aliens
+    this.alienGroup = this.add.group();
+
+    this.createAlien();
   }
 
   update(time, delta) {
-
-    const keyLeftObj = this.input.keyboard.addKey('LEFT')
-    const keyRightObj = this.input.keyboard.addKey('RIGHT')
-    const keySpaceObj = this.input.keyboard.addKey('SPACE')
+    const keyLeftObj = this.input.keyboard.addKey("LEFT");
+    const keyRightObj = this.input.keyboard.addKey("RIGHT");
+    const keySpaceObj = this.input.keyboard.addKey("SPACE");
 
     if (keyLeftObj.isDown === true) {
-      this.ship.x -= 15
+      this.ship.x -= 15;
       if (this.ship.x < 0) {
-        this.ship.x = 0
+        this.ship.x = 0;
       }
     }
 
     if (keyRightObj.isDown === true) {
-      this.ship.x += 15
+      this.ship.x += 15;
       if (this.ship.x > 1920) {
-        this.ship.x = 1920
+        this.ship.x = 1920;
       }
     }
 
     if (keySpaceObj.isDown === true) {
       if (this.fireMissile === false) {
-        this.fireMissile = true
-        const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, 'missile')
-        this.missileGroup.add(aNewMissile)
-        this.sound.play('laser')
+        this.fireMissile = true;
+        const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, "missile");
+        this.missileGroup.add(aNewMissile);
+        this.sound.play("laser");
       }
     }
 
     if (keySpaceObj.isUp === true) {
-      this.fireMissile = false
+      this.fireMissile = false;
     }
-    this.missileGroup.children.each((item) => {
-      item.y = item.y - 15
+
+    this.missileGroup.children.each(function (item) {
+      item.y = item.y - 15;
       if (item.y < 0) {
-        item.destroy()
+        item.destroy();
       }
-    })
+    });
   }
 }
 
-export default GameScene
+export default GameScene;
